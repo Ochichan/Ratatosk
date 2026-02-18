cf930a8804383ee957b7c45b9e7c8afa17aea5ce
Chisung Oh <ohstar94@gmail.com>
2026-02-19 08:52:42 +0900

    Revert "perf: additional performance fixes"
    This reverts commit 8b186c2650d58bb818bf12863ec197014522cd5b.

--- .cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/leptos_macro-0.7.9/tests/ui.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(not(erase_components))]
-#[test]
-fn ui() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/component.rs");
-    t.compile_fail("tests/ui/component_absolute.rs");
-    t.compile_fail("tests/ui/server.rs");
-}
--- .cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/serde_cow-0.1.2/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-## 0.1.2
-
-- Fix support for `no_std`.
-
-## 0.1.1
-
-- Documented MSRV as 1.56.
-- Added support for `no_std`.
